24574ae20ac627c213df1d2c6f15693d88a0691a
PascalSun <pascalsun23@gmail.com>
2018-06-19 16:48:36 +1000

    update the requirement file

--- heroku-deployment.sh
+++ heroku-deployment.sh
@@ -1,6 +1,12 @@
+# Login: first time when you deploy it
+# heroku login
+# heroku git:remote -a shedafullstackdemo # connect the project with the repo
+
 cd backend
 source env/bin/activate
 tox
+
+# python manage.py collectstatic --noinput # first time to deployment
 cd ..
 heroku config:set DJANGO_SETTINGS_MODULE=todo.settings.prod
 git subtree push --prefix backend heroku master
