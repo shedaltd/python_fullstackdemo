d806774b68a0838d877b15cfa98b36b2707e1e6e
PascalSun <pascalsun23@gmail.com>
2018-06-19 16:40:27 +1000

    update config file

--- heroku-deployment.sh
+++ heroku-deployment.sh
@@ -1,2 +1,8 @@
+cd backend
+source env/bin/activate
+tox
+cd ..
 heroku config:set DJANGO_SETTINGS_MODULE=todo.settings.prod
 git subtree push --prefix backend heroku master
+heroku run python manage.py migrate
+# heroku run ./run.sh # run it if deploy for the first time
