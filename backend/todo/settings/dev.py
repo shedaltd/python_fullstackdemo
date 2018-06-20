4aff044275d2f2009a6208d3e5ee0486adbf15bc
PascalSun <pascalsun23@gmail.com>
2018-06-20 16:40:19 +1000

    docker backend

--- backend/todo/settings/dev.py
+++ backend/todo/settings/dev.py
@@ -6,3 +6,18 @@ from .base import *
 DEBUG = True
 INTERNAL_IPS = '127.0.0.1'
 ALLOWED_HOSTS = ['127.0.0.1','localhost', 'backend']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'DB': 'dev',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': 5432,
+    }
+}
+
+DB_HOST = os.getenv('DB_HOST', 'localhost')
+DATABASES['default']['HOST'] = DB_HOST
