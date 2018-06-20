9e4bb68bfd20b1352cbe44561dbf4c641e60886b
PascalSun <pascalsun23@gmail.com>
2018-06-20 15:39:28 +1000

    setup docker

--- backend/run.sh
+++ backend/run.sh
@@ -1,8 +1,8 @@
-. env/bin/activate 
+. ../env/bin/activate 
 
 if [ ! -f db.sqlite3 ]; then
     python manage.py migrate
     echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin','admin@local.test', 'password123')" | python manage.py shell
 fi
-
+cd ./backend
 python manage.py runserver 0.0.0.0:8000
---
+++ frontend/start.sh
@@ -0,0 +1,1 @@
+$HOME/.yarn/bin/yarn start
